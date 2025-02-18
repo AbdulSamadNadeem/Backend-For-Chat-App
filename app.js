097536e0559f5d783f60e18233742a9e9823e633
AbdulSamadNadeem <samadnadeem372@gmail.com>
2025-02-18 12:38:34 +0500

    app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const server = http.createServer(app)
 const io = require('socket.io')(server , {
     cors:{
         origin:'http://localhost:5173',
-        methods:['GET','POST' , 'PUT],
+        methods:['GET','POST' , 'PUT'],
         credentials: true          
         
     }
