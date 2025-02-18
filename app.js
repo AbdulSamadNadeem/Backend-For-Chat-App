e3317cd4b93db6f8ebc97333b8838fd4907d1586
AbdulSamadNadeem <samadnadeem372@gmail.com>
2025-02-18 14:56:05 +0500

    Update app.js

--- app.js
+++ app.js
@@ -11,9 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-app.use(cors(
-  {origin:'https://chit-shat.vercel.app'}
-));
+app.use(cors());
 
 
 app.use(morgan("dev"));
